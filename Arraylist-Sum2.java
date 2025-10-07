c5f3a56f506f5bdc13a887d6e124ec6ef2963487
milnee <singh-m21@ulster.ac.uk>
2025-10-07 15:16:40 +0100

    done

---
+++ Arraylist-Sum2.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class Solution {
+    public static void main(String[] args) {
+        // ArrayList of fruits to be used in fruit salad
+        ArrayList<String> fruits= new ArrayList<>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("kiwi");
+        fruits.add("mango");
+
+        // TODO: Loop over the ArrayList of fruits and add them to the fruit salad with a print statement
+        for (String fruit : fruits) {
+            
+            // TODO: Print a statement for adding each fruit to the fruit salad.
+            System.out.println("Adding " + fruit + " to the fruit salad.");
+            
+            
+        }
+        }
+}
